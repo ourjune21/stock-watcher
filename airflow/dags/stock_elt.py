c0a45024e390076c28e6f64b5bb58d8f3e831018
Proverbs1603 <tlsdnd1667@gmail.com>
2024-11-28 12:49:34 +0900

    ELT dag operater update & write down comment how to use ELT trigger dag

--- airflow/dags/stock_elt.py
+++ airflow/dags/stock_elt.py
@@ -16,7 +16,7 @@ def check_dbt_results(context):
 conn = BaseHook.get_connection('snowflake_conn')
 
 with DAG(
-    dag_id = 'cryptocurrencies_elt',
+    dag_id = 'stock_elt',
     start_date=datetime(2024, 11, 28),
     description='A sample Airflow DAG to invoke dbt runs using a BashOperator',
     schedule=None,
@@ -38,16 +38,38 @@ with DAG(
     dbt_deps = BashOperator(
         task_id='dbt_deps',
         bash_command=f"/home/airflow/.local/bin/dbt deps --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
+        trigger_rule="all_success",
     )
 
     dbt_run = BashOperator(
         task_id="dbt_run",
         bash_command=f"/home/airflow/.local/bin/dbt run --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
+        trigger_rule="all_success",
     )
 
     dbt_test = BashOperator(
         task_id="dbt_test",
         bash_command=f"/home/airflow/.local/bin/dbt test --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
+        trigger_rule="all_success",
     )
 
     dbt_deps >> dbt_run >> dbt_test
+
+
+'''
+사용방법
+1. 각 dag 파일에 다음과 같이 TriggerDagRunOperator를 구현하고 task_id는 'trigger_stock_elt' 로 그대로 놔둔다. (같은 dag 파일에서만 task_id가 겹치지만 않으면 된다.)
+2. trigger_dag_id는 'stock_elt' 도 그대로 놔둔다.
+3. 마지막 작업으로 >> trigger_stock_elt 를 호출하여 dbt로 elt를 트리거한다.
+
+    trigger_cryptocurrencies_elt = TriggerDagRunOperator(
+        task_id='trigger_stock_elt',
+        trigger_dag_id='stock_elt',
+        wait_for_completion=False,
+    )
+
+ex)
+    raw_data = extract(symbols)
+    transformed_data = transform(raw_data)
+    load(cursor, transformed_data, target_table) >> trigger_stock_elt
+'''
